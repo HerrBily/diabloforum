0a50864f93ac72fbb6a026549ef5ccd6fcba6d5f
Fabio Bily <fabio.bily93@gmail.com>
2020-02-03 02:54:21 +0100

    styling

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,39 +25,36 @@
             </div>
 
             <div class="textfield{{ $errors->has('password') ? ' has-error' : '' }}">
-                    <label for="password" class="">Passwort</label>
+                <label for="password" class="">Passwort</label>
 
-                    <input id="password" type="password" placeholder="Passwort" name="password" required>
+                <input id="password" type="password" placeholder="Passwort" name="password" required>
 
-                    @if ($errors->has('password'))
-                    <span class="help-block">
-                        <strong>{{ $errors->first('password') }}</strong>
-                    </span>
-                    @endif
-               
+                @if ($errors->has('password'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('password') }}</strong>
+                </span>
+                @endif
+
             </div>
 
-            <div class="form-group">
-                <div class="col-md-6 col-md-offset-4">
-                    <div class="checkbox">
-                        <label>
-                            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                        </label>
-                    </div>
-                </div>
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> 
+                    Erinnere dich
+                </label>
             </div>
 
-            <div class="form-group">
-                <div class="col-md-8 col-md-offset-4">
-                    <button type="submit" class="btn btn-primary">
-                        Login
-                    </button>
 
-                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                        Forgot Your Password?
-                    </a>
-                </div>
+            <div class="login_btn">
+                <button type="submit">
+                    Login
+                </button>
+
+                <a href="{{ route('password.request') }}">
+                    Passwort vergessen?
+                </a>
             </div>
+
         </form>
 
     </div>
